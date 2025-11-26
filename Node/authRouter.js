79e4c580d3769a1fbeca06cdd047ba7e2fda49e7
Taehyeon88 <Taehyeon5376@naver.com>
2025-11-27 00:20:25 +0900

    [Node Upd] 로그인, 회원가입, 로그아웃 쿼리문 추가

--- Node/authRouter.js
+++ Node/authRouter.js
@@ -2,6 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
+const mysql = require('mysql2/promise');
+
+const pool = mysql.createPool({
+    host : 'localhost',
+    user : 'root',
+    password : '112233',
+    database : 'gametest'
+});
+
 
 //Express 앱 생성 및 미들웨어 설정
 const router = express.Router();
@@ -17,16 +26,36 @@ const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
 
 router.post('/register', async(req, res) => {
 
-    const {username, password} = req.body;
-    console.log("작동해용ㅇ");
+    const {username, password, nickname} = req.body;
 
-    if(users.find(user => user.username === username))
+    try
     {
-        return res.status(400).json({error : '이미 존재하는 사용자 입니다.'});
-    }
+        const [temp1] = await pool.query(
+           'SELECT COUNT(*) AS count FROM users WHERE username = ?', [username]
+        );
+        const [temp2] = await pool.query(
+           'SELECT COUNT(*) AS count FROM users WHERE nickname = ?', [nickname]
+        );
 
-    const hashedPassword = await bcrypt.hash(password, 10);
-    users.push({username, password: hashedPassword});        //hash값으로 비밀번호
+        if(temp1[0].count == 1)
+        {
+            return res.status(400).json({error : '이미 사용중인 사용자이름입니다.'});
+        }
+        else if (temp2[0].count == 1)
+        {
+            return res.status(400).json({error : '이미 사용중인 닉네임입니다.'});
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        await pool.query(                                             //회원 정보 업데이트
+            'INSERT INTO users (username, passward, nickname) VALUES (?, ?, ?)',
+            [username, hashedPassword, nickname]
+        );
+    }
+    catch(error)
+    {
+        res.status(500).json({success: false, message : error.message});
+    }
 
     res.status(201).json({message : '회원 가입 성공'});
 })
@@ -34,20 +63,79 @@ router.post('/register', async(req, res) => {
 //로그인 라우트
 router.post('/login', async(req, res) => {
     const {username, password} = req.body;
-    const user = users.find(user => user.username === username);
-
-    if(!user || !(await bcrypt.compare(password, user.password)))
+    
+    try
     {
-        return res.status(400).json({error : '잘못된 사용자면 또는 비밀번호입니다.'});
+        const [user] = await pool.query(
+           'SELECT user_id, passward, nickname FROM users WHERE username = ?',
+           [username]
+        );
+
+        if(user.length <= 0 || !(await bcrypt.compare(password, user[0].passward)))
+        {
+            return res.status(400).json({error : '잘못된 사용자면 또는 비밀번호입니다.'});
+        }
+
+        const [temp] = await pool.query(
+            'SELECT COUNT(*) AS count FROM refresh_tokens WHERE user_id = ?',
+            [user[0].user_id]
+        );
+        if(temp[0].count == 1)
+        {
+            return res.status(400).json({error : '이미 로그인이된 계정입니다.'});
+        }
+
+        user_id = user[0].user_id;    
+        await pool.query(
+            'UPDATE users SET logined_at = NOW() WHERE user_id = ?',
+            [user_id]
+        );
+
+        const accessToken = generateAccessToken(username);
+        //console.log(accessToken);
+        const refreschToken = jwt.sign({username}, REFRESH_TOKEN_SECRET);
+
+        await pool.query(
+            'UPDATE users SET logined_at = NOW() WHERE user_id = ?',
+            [user_id]
+        );
+        
+        await pool.query(
+            'INSERT INTO refresh_tokens (user_id, refresh_token) VALUES (?, ?)',
+            [user_id, refreschToken]
+        ); 
+
+        res.json({user_id, accessToken, refreschToken});
     }
+    catch(error)
+    {
+        res.status(500).json({success: false, message : error.message});
+    }
+});
 
-    const accessToken = generateAccessToken(username);
-    console.log(accessToken);
-    const refreschToken = jwt.sign({username}, REFRESH_TOKEN_SECRET);
+//로그인 라우트
+router.post('/logout', async(req, res) => {
+    const {user_id} = req.body;
 
-    refreshTokens[refreschToken] = username;
-    res.json({accessToken, refreschToken});
-})
+    try
+    {       
+        await pool.query(
+            'UPDATE users SET logouted_at = NOW() WHERE user_id = ?',
+            [user_id]
+        );
+        
+        await pool.query(
+            'DELETE FROM refresh_tokens WHERE user_id = ?',
+            [user_id]
+        );
+
+        res.status(201).json({message : '로그아웃 되었습니다.'});
+    }
+    catch(error)
+    {
+        res.status(500).json({success: false, message : error.message});
+    }
+});
 
 //액세스 토큰 생성 함수
 function generateAccessToken(username)
@@ -71,8 +159,8 @@ function authenticateToken(req, res, next) {
 router.get('/token', async(req, res) => {
     const{accessToken, refreschToken} = req.body;
 
-    if(accessToken == null) return res.sendStatus(401);
-    if(refreschToken == null) return res.sendStatus(401);
+    if(!accessToken) return res.sendStatus(401);
+    if(!refreschToken) return res.sendStatus(401);
 
     jwt.verify(accessToken, JWT_SECRET, (err, user) =>{
         if(err)
@@ -81,16 +169,33 @@ router.get('/token', async(req, res) => {
 
             jwt.verify(refreschToken, REFRESH_TOKEN_SECRET, (err, user) => {
                 if(err) return res.sendStatus(403);
+                try
+                {
+                    const [user_id] = await.pool.query(
+                        'SELECT user_id FROM refresh_tokens WHERE refresh_token = ?',
+                        [refreschToken]
+                    );
+                }
+                catch(error)
+                {
+                    res.status(500).json({success: false, message : error.message});
+                }
 
-                const username = refreshTokens[refreschToken];
-
-                const accessToken = generateAccessToken(username);      //접근 토큰 생성
+                if(user_id.length <= 0)
+                {
+                    return res.status(400).json({error : '데이터 베이스에서 refreshToken 데이터를 찾을 수 없습니다.'});
+                }
+                const accessToken = generateAccessToken(user_id[0]);      //접근 토큰 생성
                 console.log(accessToken);
 
                 res.json({accessToken});   //점근 토큰 전달
             })
         }
+        else
+        {
+            res.json({accessToken});
+        }
     })
-})
+});
 
 module.exports = router;  //라우터 등록
--- Node/gameServer.js
+++ Node/gameServer.js
@@ -6,7 +6,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
     host : 'localhost',
     user : 'root',
-    password : '8688',
+    password : '112233',
     database : 'gametest'
 });
     //쿼리 사용 예시
